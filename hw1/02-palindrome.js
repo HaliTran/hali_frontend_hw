6814257bd78becdd3cdc6b55e433f0c50339141b
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-04-09 21:55:45 -0700

    Add palindrome logic and eslint config

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,18 @@
 const elem = document.querySelector('input');
+const response = document.getElementById('response');
+
+const handleInput = function isPalindrome() {
+  const numAsString = elem.value;
+
+  if (isNaN(numAsString) || !numAsString) {
+    response.innerHTML = '';
+  } else if (Number(numAsString) < 0) {
+    response.innerHTML = 'Invalid input! Please enter a positive number';
+  } else if (numAsString === numAsString.split('').reverse().join('')) {
+    response.innerHTML = 'Yes. This is a palindrome!';
+  } else {
+    response.innerHTML = 'No. Try again!';
+  }
+};
 
 elem.addEventListener('input', handleInput);
