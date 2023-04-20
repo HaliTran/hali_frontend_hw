260ab0f86f2ee9f8627900974ee3b56b362504c1
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-04-20 16:50:47 -0700

    Finish execise 04

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,6 +1,8 @@
 const bodyElem = document.querySelector('body');
 const button = document.getElementById('button');
 const intervalInput = document.getElementById('interval');
+let repeatingColor = true;
+let intervalID;
 
 const changeBackground = function changeBackgroundColor() {
   let redColor = Math.floor(Math.random() * 256);
@@ -14,10 +16,24 @@ const changeBackground = function changeBackgroundColor() {
 const repeatChangeBackground = function repeatChangeBackgroundColor() {
   const { value } = intervalInput;
   const interval = value * 1000;
-  setInterval(changeBackground, interval);
+  intervalID = setInterval(changeBackground, interval);
 };
 
 window.addEventListener('load', () => {
   bodyElem.style.backgroundColor = 'green';
   repeatChangeBackground();
+});
+
+button.addEventListener('click', () => {
+  if (repeatingColor) {
+    repeatingColor = false;
+    button.setAttribute('class', 'btn btn-primary');
+    button.innerText = 'Start';
+    clearInterval(intervalID);
+  } else {
+    repeatingColor = true;
+    button.setAttribute('class', 'btn btn-danger');
+    button.innerText = 'Stop';
+    repeatChangeBackground();
+  }
 });
