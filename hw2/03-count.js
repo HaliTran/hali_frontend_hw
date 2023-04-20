d31e8e84554f4ac6ea3d7271330cde6bb82adc07
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-04-19 21:04:31 -0700

    Word match complete except for words preceding special character

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,6 +1,22 @@
 const pattern = document.querySelector('input');
 const text = document.getElementById('given-text');
+const textArray = text.innerText.split(' ');
+console.log(text.innerText);
+console.log(textArray);
 
-const userInput = function checkUserInput() {};
+const checkPattern = function checkMatchingPattern() {
+  const { value } = pattern;
+  let textToReplace = [];
 
-pattern.addEventListener('keydown', userInput);
+  for (let i = 0; i < textArray.length; ++i) {
+    if (value === textArray[i]) {
+      textToReplace[i] = `<span class="bg-success">${value}</span>`;
+    } else {
+      textToReplace[i] = textArray[i];
+    }
+  }
+  textToReplace = textToReplace.join(' ');
+  text.innerHTML = textToReplace;
+};
+
+pattern.addEventListener('keyup', checkPattern);
