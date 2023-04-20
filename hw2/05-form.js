b3a6d690451965d184101e04c67331561ca03340
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-04-19 18:08:53 -0700

    Finish form exercise

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,24 @@
-// Add your code here
+const myForm = document.getElementById('myForm');
+const fullName = document.getElementById('name');
+const email = document.getElementById('email');
+const regStatus = document.getElementById('status');
+const info = document.getElementById('extra-info');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+myForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  console.log(`Full Name: ${fullName.value}`);
+  console.log(`Email: ${email.value}`);
+  console.log(`Registration Status: ${regStatus.value}`);
+  console.log(`Have you taken any of the following courses?`);
+
+  checkboxes.forEach((checkbox) => {
+    const { value } = checkbox;
+    if (checkbox.checked) {
+      console.log(value);
+    }
+  });
+
+  console.log(`Anything else: ${info.value}`);
+});
