9d503fe17863ff04f3b1e498a2c01e91511f410a
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-04-20 15:54:10 -0700

    Exercise 04: Add function to change bg color

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,23 @@
-// Add your code here
+const bodyElem = document.querySelector('body');
+const button = document.getElementById('button');
+const intervalInput = document.getElementById('interval');
+
+const changeBackground = function changeBackgroundColor() {
+  let redColor = Math.floor(Math.random() * 256);
+  let greenColor = Math.floor(Math.random() * 256);
+  let blueColor = Math.floor(Math.random() * 256);
+  let alpha = Math.random();
+
+  bodyElem.style.backgroundColor = `rgba(${redColor},${greenColor},${blueColor},${alpha})`;
+};
+
+const repeatChangeBackground = function repeatChangeBackgroundColor() {
+  const { value } = intervalInput;
+  const interval = value * 1000;
+  setInterval(changeBackground, interval);
+};
+
+window.addEventListener('load', () => {
+  bodyElem.style.backgroundColor = 'green';
+  repeatChangeBackground();
+});
