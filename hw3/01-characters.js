8edaed72b4d3f27103abb12f874905eff3201ece
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-04-28 18:10:00 -0700

    Fetch throne data and convert to json

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,10 @@
-// url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-fetch(url);
+// retrieve throne data as array of json object
+const getData = function getData() {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => resolve(data));
+  });
+};
