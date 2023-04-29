72a9f4aeb55f5adefcbdeab17f5ab43b349a520d
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-04-28 18:52:05 -0700

    Create DOM element for each character

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,10 +1,36 @@
 const url = 'https://thronesapi.com/api/v2/Characters';
+const container = document.getElementById('container');
 
-// retrieve throne data as array of json object
 const getData = function getData() {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => resolve(data));
+  });
+};
+
+const createElements = function createThroneElements() {
+  const throneElement = getData();
+  throneElement.then((data) => {
+    data.forEach((element) => {
+      const parentElement = document.createElement('div');
+      const imgElement = document.createElement('img');
+      const nameElement = document.createElement('h4');
+      const titleElement = document.createElement('h5');
+
+      imgElement.src = element.imageUrl;
+      imgElement.setAttribute(
+        'alt',
+        `photo of GOT character ${element.fullName}`
+      );
+      nameElement.innerText = element.fullName;
+      titleElement.innerText = element.title;
+
+      parentElement.appendChild(imgElement);
+      parentElement.appendChild(nameElement);
+      parentElement.appendChild(titleElement);
+
+      container.appendChild(parentElement);
+    });
   });
 };
